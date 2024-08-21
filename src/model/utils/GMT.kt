66b635a18a546a1b690584c01c030eac44049aa6
igorcardosoy <igorfilipicardoso@gmail.com>
2024-08-21 15:57:30 -0300

    fix: str of GMT Enum

--- src/model/utils/GMT.kt
+++ src/model/utils/GMT.kt
@@ -1,17 +1,17 @@
 package model.utils
 
-enum class GMT(val s: String) {
-    GMT_5("model.Utils.GMT-5"),
-    GMT_4("model.Utils.GMT-4"),
-    GMT_3("model.Utils.GMT-3"),
-    GMT_2("model.Utils.GMT-2"),
-    GMT_1("model.Utils.GMT-1"),
-    GMT0("model.Utils.GMT"),
-    GMT1("model.Utils.GMT+1"),
-    GMT2("model.Utils.GMT+2"),
-    GMT3("model.Utils.GMT+3"),
-    GMT4("model.Utils.GMT+4"),
-    GMT5("model.Utils.GMT+5");
+enum class GMT(private val s: String) {
+    GMT_5("GMT-5"),
+    GMT_4("GMT-4"),
+    GMT_3("GMT-3"),
+    GMT_2("GMT-2"),
+    GMT_1("GMT-1"),
+    GMT0("GMT"),
+    GMT1("GMT+1"),
+    GMT2("GMT+2"),
+    GMT3("GMT+3"),
+    GMT4("GMT+4"),
+    GMT5("GMT+5");
 
     fun getString(): String {
         return s
