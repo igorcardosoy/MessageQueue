89edbc4aad476537f23f24bc4ba271739207968c
igorcardosoy <igorfilipicardoso@gmail.com>
2024-09-07 18:01:51 -0300

    fix: removing synchronized block.

--- src/model/messageQueue/MessageQueue.kt
+++ src/model/messageQueue/MessageQueue.kt
@@ -7,20 +7,16 @@ class MessageQueue {
     private val queues = ConcurrentHashMap<String, LinkedBlockingQueue<Message>>()
 
     fun publish(message: Message): Boolean {
-        synchronized(this){
             val queue = queues.getOrPut(message.local) {
                LinkedBlockingQueue(10)
            }
 
             return queue.offer(message)
-        }
     }
 
     fun subscribe(local: String): LinkedBlockingQueue<Message> {
-        synchronized(this){
             return queues.getOrPut(local) {
                 LinkedBlockingQueue(10)
             }
-        }
     }
 }
